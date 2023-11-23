2529f9cb48fe6a2bea02cf1c274dee53354b3cc8
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-23 12:10:57 +0100

    structure ++models ++post endpoint

--- src/api/endpoints/wc.py
+++ src/api/endpoints/wc.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 from pymongo import MongoClient
 
-from src.api.models.wc import wc
+from src.api.models.wc import WC
 
 
 # TODO: URI & Name as Docker env vars
-MONGODB_URI = "mongodb://localhost:27017"
+MONGODB_URI = "mongodb://localhost:27016"
 DATABASE_NAME = "mekgo"
 
 client = MongoClient(MONGODB_URI)
@@ -22,8 +22,8 @@ def read_wc(wc_id: int):
     return {"message": f"Read item with ID: {wc_id}"}
 
 @router.post("/insert_wc/")
-def insert_item(wc: wc):
-    collection = db.items
+def insert_item(wc: WC):
+    collection = db.wcs
     inserted_wc = collection.insert_one(wc.dict())
     
     return {"inserted_id": str(inserted_wc.inserted_id)}
