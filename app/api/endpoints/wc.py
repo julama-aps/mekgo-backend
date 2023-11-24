707010f2e7cd05d20d579c7d692dc5be3b341487
R-dVL <rauldel.valle.lima@gmail.com>
2023-11-24 12:54:10 +0100

    Finished API Routes & Model

--- app/api/endpoints/wc.py
+++ app/api/endpoints/wc.py
@@ -1,46 +1,105 @@
-from fastapi import APIRouter
-from pymongo import MongoClient
+from fastapi import APIRouter, Body, HTTPException, status
 
-from app.api.models.wc import WC
+import motor.motor_asyncio
+from bson import ObjectId
+from pymongo import ReturnDocument
+from fastapi.responses import Response
+
+from app.api.models.wc import WCModel, UpdateWCModel, WCCollection
 
 
 # TODO: URI & Name as Docker env vars
 MONGODB_URI = "mongodb://192.168.1.33:27016"
 DATABASE_NAME = "mekgo"
 
-client = MongoClient(MONGODB_URI)
-db = client[DATABASE_NAME]
-collection = db['wcs']
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
+db = client.mekgo
+wcs_collection = db.get_collection("wcs")
 
 router = APIRouter()
 
 #### CRUD ####
 ## Create ##
 # Post WC
-@router.post("/new")
-def insert_wc(wc: WC):
-    inserted_wc = collection.insert_one(wc.dict())
-    return {"inserted_id": str(inserted_wc.inserted_id)}
+@router.post(
+    "/new/",
+    response_description="Add new wc",
+    response_model = WCModel,
+    status_code = status.HTTP_201_CREATED,
+    response_model_by_alias=False,
+)
+async def create_wc(wc: WCModel = Body(...)):
+    new_wc = await wcs_collection.insert_one(
+        wc.model_dump(by_alias=True, exclude=["id"])
+    )
+    created_wc = await wcs_collection.find_one(
+        {"_id": new_wc.inserted_id}
+    )
+    return created_wc
 
 ## Read ##
 # Get All WCs
-@router.get("/")
-def read_wcs():
-    return {"message": "Read all items"}
+@router.get(
+    "/",
+    response_description="List all wcs",
+    response_model=WCCollection,
+    response_model_by_alias=False,
+)
+async def list_wcs():
+    return WCCollection(wcs=await wcs_collection.find().to_list(1000))
 
 # Get Specific WC
-@router.get("/{wc_id}")
-def read_wc(wc_id: str):
-    return {"message": f"Read wc with ID: {wc_id}"}
+@router.get(
+    "/{id}",
+    response_description="Get a single wc",
+    response_model=WCModel,
+    response_model_by_alias=False,
+)
+async def show_wc(id: str):
+    if (
+        wc := await wcs_collection.find_one({"_id": ObjectId(id)})
+    ) is not None:
+        return wc
+
+    raise HTTPException(status_code=404, detail=f"WC {id} not found")
 
 ## Update ##
 # Update Specific WC
-@router.put("/modify/{wc_id}")
-def modify_wc(wc_id: str):
-    return {"message": f"Modify wc with ID: {wc_id}"}
+@router.put(
+    "/update/{id}",
+    response_description="Update a wc",
+    response_model=WCModel,
+    response_model_by_alias=False,
+)
+async def update_wc(id: str, wc: UpdateWCModel = Body(...)):
+    wc = {
+        k: v for k, v in wc.model_dump(by_alias=True).items() if v is not None
+    }
+
+    if len(wc) >= 1:
+        update_result = await wcs_collection.find_one_and_update(
+            {"_id": ObjectId(id)},
+            {"$set": wc},
+            return_document=ReturnDocument.AFTER,
+        )
+        if update_result is not None:
+            return update_result
+        else:
+            raise HTTPException(status_code=404, detail=f"WC {id} not found")
+
+    # The update is empty, but we should still return the matching document:S
+    if (existing_wc := await wcs_collection.find_one({"_id": id})) is not None:
+        return existing_wc
+
+    raise HTTPException(status_code=404, detail=f"WC {id} not found")
 
 ## Delete ##
 # Delete Specific WC
-@router.delete("/delete/{wc_id}")
-def delete_wc(wc_id: str):
-    return {"message": f"Delete wc with ID: {wc_id}"}
+@router.delete("/{id}", response_description="Delete a WC")
+async def delete_wc(id: str):
+    delete_result = await wcs_collection.delete_one({"_id": ObjectId(id)})
+
+    if delete_result.deleted_count == 1:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"WC {id} not found")
